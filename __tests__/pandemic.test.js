5993db1ce16f783bb36a1dde628864b496a8e59a
Mikah Mathews <mikah@outofband.org>
2020-04-20 14:39:53 -0700

    Add test for city values

--- __tests__/pandemic.test.js
+++ __tests__/pandemic.test.js
@@ -1,61 +1,58 @@
 import {City} from '../src/city'
 
 describe('', () => {
-  let seattle, philadelphia, bangkok, wuhan, rome, venice, paris;
+  //let seattle, philadelphia, bangkok, wuhan, rome, venice, paris, virusTimer;
   beforeEach(() => {
-    seattle = new City("Seattle", 3);
-    philadelphia = new City("Philadelphia", 2);
-    bangkok = new City("Bangkok", 1);
-    wuhan = new City("Wuhan", 0);
-    rome = new City("Rome", 0);
-    venice = new City("Venice", 0);
-    paris = new City("Paris", 0);
-    cityArray = [seattle, philadelphia, bangkok, wuhan, rome, venice, paris]
+    let cityConstruct = new City;
+    let testCities = cityConstruct.createCities();
+    // virusTimer = 
   });
-  test('should ...', () => {});
-
 
   test('Seattle infection should be 3, Philadelphia infection should be 2, Bangkok infection should be 1, and Wuhan City, Rome, Venice, and Paris infection rates should be 0', () => {
-    expect(cityArray[0]).toEqual("Seattle", 3);
-    expect(cityArray[1]).toEqual("Philadelphia", 2);
-    expect(cityArray[2]).toEqual("Bangkok", 1);
-    expect(cityArray[3]).toEqual("Wuhan", 0);
-    expect(cityArray[4]).toEqual("Rome", 0);
-    expect(cityArray[5]).toEqual("Venice", 0);
-    expect(cityArray[6]).toEqual("Paris", 0);
+    let cityConstruct = new City;
+    let testCities = cityConstruct.createCities();
+    expect(testCities[0]).toEqual(["Seattle", 3, false]);
+    expect(testCities[1]).toEqual(["Philadelphia", 2, false]);
+    expect(testCities[2]).toEqual(["Bangkok", 1, false]);
+    expect(testCities[3]).toEqual(["Wuhan", 0, false]);
+    expect(testCities[4]).toEqual(["Rome", 0, false]);
+    expect(testCities[5]).toEqual(["Venice", 0, false]);
+    expect(testCities[6]).toEqual(["Paris", 0, false]);
  }); 
 
- test('new city is infected when prev. city reaches infection level 2', () => {
+  test('virus value increases after x time duration', () => {
+    let cityConstruct = new City;
+    let testCities = cityConstruct.createCities();
+    
+  });
 
- });
+//  test('new city is infected when prev. city reaches infection level 2', () => {
 
- test('virus value increases after x time duration', () => {
+//  });
 
- });
+//  test('virus value decreases after treatment', () => {
 
- test('virus value decreases after treatment', () => {
+//  });
 
- });
+//  test('if city is placed under quarantine, it is not infected', () => {
 
- test('if city is placed under quarantine, it is not infected', () => {
+//  });
 
- });
+//  test('vaccine has a value of 0 at start', () => {
 
- test('vaccine has a value of 0 at start', () => {
+//  });
 
- });
+//  test('vaccine has increased value when vaccine research is run', () => {
 
- test('vaccine has increased value when vaccine research is run', () => {
+//  });
 
- });
+//  test('when vaccine reaches 5, win function is run', () => {
 
- test('when vaccine reaches 5, win function is run', () => {
+//  });
 
- });
+//  test('when timer hits 0, lose function is run', () => {
 
- test('when timer hits 0, lose function is run', () => {
-
- });
+//  });
 
  
 });
--- src/city.js
+++ src/city.js
@@ -1,14 +1,60 @@
 export class City{
-  constructor(name, diseaseBlock){
-    name = this.name;
-    diseaseBlock = this.diseaseBlock;
-    quarantine = false;
+  constructor(name, diseaseBlock) {
+    this.name = name;
+    this.diseaseBlock = diseaseBlock;
+    this.quarantine = false;
   }
-  vaccineCounter (){
+  createCities(){
+    let seattle = {
+      name: "Seattle",
+      diseaseBlock: 3,
+      quarantine: false
+     };
+    let philadelphia = {
+      name: "Philadelphia",
+      diseaseBlock: 2,
+      quarantine: false
+    };
+    let bangkok = {
+      name: "Bangkok",
+      diseaseBlock: 1,
+      quarantine: false
+    };
+    let wuhan = {
+      name: "Wuhan",
+      diseaseBlock: 0,
+      quarantine: false
+    };
+    let rome = {
+      name: "Rome",
+      diseaseBlock: 0,
+      quarantine: false
+    };
+    let venice = {
+      name: "Venice",
+      diseaseBlock: 0,
+      quarantine: false
+    };
+    let paris = {
+      name: "Paris",
+      diseaseBlock: 0,
+      quarantine: false
+    };
+    let cityArray = [Object.values(seattle), Object.values(philadelphia), Object.values(bangkok), Object.values(wuhan), Object.values(rome), Object.values(venice), Object.values(paris)];
+    console.log(cityArray);
+    return cityArray;
+  };
+  vaccineCounter () {
     let vaccine = 0;
     vaccine++;
     if (vaccine === 5){
-      return "Congratulation! You've discovered the cure. You win!"
-    } else return ("Your current vaccine total is: " + vaccine);    
-  }
+      return "Congratulation! You've discovered the cure. You win!";
+    } else return ("Your current vaccine total is: " + vaccine);
+  };
+  increaseInfection(array){
+    for(i = 0; i < array.length; i++){
+      //if diseaseBlock >= 2 {increment i+1}
+      if 
+    }
+  };
 }
