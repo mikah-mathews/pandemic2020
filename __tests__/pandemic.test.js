6feb9080e32794fc97084f17539b2aad5ae15f83
Mikah Mathews <mikah@outofband.org>
2020-04-21 16:30:17 -0700

    First attempt at endgame message function.

--- __tests__/pandemic.test.js
+++ __tests__/pandemic.test.js
@@ -1,65 +1,99 @@
 import {City} from '../src/city'
 
 describe('', () => {
+  jest.useFakeTimers();
   //let seattle, philadelphia, bangkok, wuhan, rome, venice, paris, virusTimer;
+  let cityConstruct
+  let testCities
   beforeEach(() => {
-    let cityConstruct = new City;
-    let testCities = cityConstruct.createCities();
+    cityConstruct = new City;
+    testCities = cityConstruct.createCities();
+    
     // virusTimer = 
   });
 
-  test('Seattle infection should be 3, Philadelphia infection should be 2, Bangkok infection should be 1, and Wuhan City, Rome, Venice, and Paris infection rates should be 0', () => {
-    let cityConstruct = new City;
-    let testCities = cityConstruct.createCities();
-    expect(testCities[0]).toEqual(["Seattle", 3, false]);
-    expect(testCities[1]).toEqual(["Philadelphia", 2, false]);
-    expect(testCities[2]).toEqual(["Bangkok", 1, false]);
-    expect(testCities[3]).toEqual(["Wuhan", 0, false]);
-    expect(testCities[4]).toEqual(["Rome", 0, false]);
-    expect(testCities[5]).toEqual(["Venice", 0, false]);
-    expect(testCities[6]).toEqual(["Paris", 0, false]);
- }); 
-
-  test('virus value increases after x time duration', () => {
-    let cityConstruct = new City;
-    let testCities = cityConstruct.createCities();
-    testCities = cityConstruct.increaseInfection(testCities);
-    expect(testCities[0]).toEqual(["Seattle", 3, false]);
-    expect(testCities[1]).toEqual(["Philadelphia", 3, false]);
-    expect(testCities[2]).toEqual(["Bangkok", 2, false]);
-    expect(testCities[3]).toEqual(["Wuhan", 1, false]);
-    expect(testCities[4]).toEqual(["Rome", 0, false]);
-    expect(testCities[5]).toEqual(["Venice", 0, false]);
-    expect(testCities[6]).toEqual(["Paris", 0, false]);
+  afterEach(function() {
+    jest.clearAllTimers();
   });
 
-//  test('new city is infected when prev. city reaches infection level 2', () => {
+  test('Seattle infection should be 3, Philadelphia infection should be 2, Bangkok infection should be 1, and Wuhan City, Rome, Venice, and Paris infection rates should be 0', () => {
+    expect(testCities[0]).toEqual(["Seattle", 3, false, false]);
+    expect(testCities[1]).toEqual(["Philadelphia", 2, false, false]);
+    expect(testCities[2]).toEqual(["Bangkok", 1, false, false]);
+    expect(testCities[3]).toEqual(["Wuhan", 0, false, false]);
+    expect(testCities[4]).toEqual(["Rome", 0, false, false]);
+    expect(testCities[5]).toEqual(["Venice", 0, false, false]);
+    expect(testCities[6]).toEqual(["Paris", 0, false, false]);
+ }); 
 
-//  });
+  test('virus value increases Philadelphia, Bangkok, and Wuhan', () => {
+    cityConstruct.increaseInfection(testCities);
+    jest.advanceTimersByTime(1001)
+    expect(testCities[0]).toEqual(["Seattle", 3, false, false]);
+    expect(testCities[1]).toEqual(["Philadelphia", 3, false, false]);
+    expect(testCities[2]).toEqual(["Bangkok", 2, false, false]);
+    expect(testCities[3]).toEqual(["Wuhan", 1, false, false]);
+    expect(testCities[4]).toEqual(["Rome", 0, false, false]);
+    expect(testCities[5]).toEqual(["Venice", 0, false, false]);
+    expect(testCities[6]).toEqual(["Paris", 0, false, false]);
+  });
 
-//  test('virus value decreases after treatment', () => {
+  test('virus value increases after 3 seconds time duration', () => {
+    cityConstruct.increaseInfection(testCities);
+    jest.advanceTimersByTime(2001)
+    expect(testCities[0]).toEqual(["Seattle", 3, false, false]);
+    expect(testCities[1]).toEqual(["Philadelphia", 3, false, false]);
+    expect(testCities[2]).toEqual(["Bangkok", 3, false, false]);
+    expect(testCities[3]).toEqual(["Wuhan", 2, false, false]);
+    expect(testCities[4]).toEqual(["Rome", 1, false, false]);
+    expect(testCities[5]).toEqual(["Venice", 0, false, false]);
+    expect(testCities[6]).toEqual(["Paris", 0, false, false]);
+  });
 
-//  });
+  test('virus value decreases after treatment', () => {   
+    cityConstruct.increaseInfection(testCities);
+    testCities[4][3] = true;
+    cityConstruct.treatment(testCities);
+    jest.advanceTimersByTime(2001);
+    expect(testCities[0]).toEqual(["Seattle", 3, false, false]);
+    expect(testCities[1]).toEqual(["Philadelphia", 3, false, false]);
+    expect(testCities[2]).toEqual(["Bangkok", 3, false, false]);
+    expect(testCities[3]).toEqual(["Wuhan", 2, false, false]);
+    expect(testCities[4]).toEqual(["Rome", 0, false, true]);
+    expect(testCities[5]).toEqual(["Venice", 0, false, false]);
+    expect(testCities[6]).toEqual(["Paris", 0, false, false]);
+  });
 
-//  test('if city is placed under quarantine, it is not infected', () => {
+ test('if city is placed under quarantine, it is not infected', () => {
+    testCities[1][2] = true;
+    cityConstruct.increaseInfection(testCities);
+    jest.advanceTimersByTime(2001);
+    expect(testCities[0]).toEqual(["Seattle", 3, false, false]);
+    expect(testCities[1]).toEqual(["Philadelphia", 2, true, false]);
+    expect(testCities[2]).toEqual(["Bangkok", 3, false, false]);
+    expect(testCities[3]).toEqual(["Wuhan", 2, false, false]);
+    expect(testCities[4]).toEqual(["Rome", 1, false, false]);
+    expect(testCities[5]).toEqual(["Venice", 0, false, false]);
+    expect(testCities[6]).toEqual(["Paris", 0, false, false]);
+ });
 
-//  });
+ test('vaccine has increased value when vaccine research is run', () => {
+    let vaccineCount = 0;
+    let vaccine = cityConstruct.vaccineCounter(vaccineCount);
+    expect(vaccine).toEqual(1);
+ });
 
-//  test('vaccine has a value of 0 at start', () => {
+ test('when vaccine reaches 5, win function is run', () => {
+  let vaccineCount = 4;
+  let vaccine = cityConstruct.vaccineCounter(vaccineCount);
+  expect(vaccine).toEqual("Congratulation! You've discovered the cure. You win!");
+ });
 
-//  });
-
-//  test('vaccine has increased value when vaccine research is run', () => {
-
-//  });
-
-//  test('when vaccine reaches 5, win function is run', () => {
-
-//  });
-
-//  test('when timer hits 0, lose function is run', () => {
-
-//  });
+ test('when timer hits 0, lose function is run', () => {
+  let endgame = cityConstruct.endgame();
+  jest.advanceTimersByTime(120001);
+  expect(endgame).toEqual("Timer has ended. Everyone is infected. You lose!");
+ });
 
  
 });
--- src/city.js
+++ src/city.js
@@ -8,61 +8,67 @@ export class City{
     let seattle = {
       name: "Seattle",
       diseaseBlock: 3,
-      quarantine: false
+      quarantine: false,
+      treat: false
      };
     let philadelphia = {
       name: "Philadelphia",
       diseaseBlock: 2,
-      quarantine: false
+      quarantine: false,
+      treat: false
     };
     let bangkok = {
       name: "Bangkok",
       diseaseBlock: 1,
-      quarantine: false
+      quarantine: false,
+      treat: false
     };
     let wuhan = {
       name: "Wuhan",
       diseaseBlock: 0,
-      quarantine: false
+      quarantine: false,
+      treat: false
     };
     let rome = {
       name: "Rome",
       diseaseBlock: 0,
-      quarantine: false
+      quarantine: false,
+      treat: false
     };
     let venice = {
       name: "Venice",
       diseaseBlock: 0,
-      quarantine: false
+      quarantine: false,
+      treat: false
     };
     let paris = {
       name: "Paris",
       diseaseBlock: 0,
-      quarantine: false
+      quarantine: false,
+      treat: false
     };
     let cityArray = [Object.values(seattle), Object.values(philadelphia), Object.values(bangkok), Object.values(wuhan), Object.values(rome), Object.values(venice), Object.values(paris)];
     return cityArray;
   };
-  vaccineCounter () {
-    let vaccine = 0;
+
+  vaccineCounter(vaccine) {
     vaccine++;
     if (vaccine === 5){
       return "Congratulation! You've discovered the cure. You win!";
-    } else return ("Your current vaccine total is: " + vaccine);
+    } else {
+      return vaccine
+    };
   };
 
   increaseInfection(array){
+    setInterval(() => {
     let returnArray = [];
     for(let i = 0; i < array.length; i++){
       let keyPairs = Object.entries(array[i]);
       let infection = Object.entries(array[i]).map(([key, value]) => [key, value + 1]);
-      console.log(infection);
       if((keyPairs[2][0] ===  '2' && keyPairs[2][1] === true) || (keyPairs[1][0] === '1' && keyPairs[1][1] === 3)) { // if ((keyPairs[2][0] === '2' && keyPairs[2][1] === true) || keyPairs[1][0] )
-        console.log( array[i] + " inside first if");
         returnArray.push(array[i]);
       } else if((keyPairs[1][1] === 2) || (keyPairs[1][1] ===  1)) {
-          console.log(array[i] + " Inside second if");
-          console.log(infection[1]); // set number to value
           array[i][1] = infection[1][1]
           returnArray.push(array[i]);
       } else if(keyPairs[1][0] === '1' && keyPairs[1][1] === 0) {
@@ -75,11 +81,49 @@ export class City{
           }
       }   
     }
-    console.log(returnArray);
     return returnArray;
+    }, 1000);
   };
+
+  treatment(cityArray){ // target specific city somehow 
+    setTimeout(() => {   
+      for(let i = 0; i < cityArray.length; i++) {
+        let keyPairs = Object.entries(cityArray[i]);
+        let treat = Object.entries(cityArray[i]).map(([key, value]) => [key, value - 1]);
+        if (keyPairs[3][1] === true) {
+          cityArray[i][1] = treat[1][1];
+          return cityArray;
+        } else {
+        }
+      }
+    }, 2000);
+  }
+
+  timer(start){ // use to display countdown on page??
+    console.log("timer has started")
+    let display = 0;
+    setInterval(() => {
+      if(start === true) {
+        for(let i = 0; i <= 120; i++) {
+          display = i;
+        }
+      } else { };
+    }, 1000);
+  }
+
+  endgame(){
+    console.log("Start of endgame");
+    let endMessage = "Timer has ended. Everyone is infected. You lose!";
+    setTimeout(() => {
+      console.log("Timer has ended");
+      return endMessage;
+    }, 120000);
+  } 
+};
+ 
+
+
+
+async function endgameMessage() {
+  
 }
-
-
-// Something like this for returning entry
-// Object.entries(prices).map(([key, value]) => [key, value * 2]) 
